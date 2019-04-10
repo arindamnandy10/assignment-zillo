b96b9e3b3a2b6b8b264283285d9a89aee753df4b
arindamnandy10 <arindamnandy10@gmail.com>
2019-04-10 09:36:27 +0530

    Latest Update

--- app.js
+++ app.js
@@ -1,24 +1,63 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const multer = require('multer');
+const crypto = require('crypto');
+const mongoose = require('mongoose');
+const GridFsStorage = require('multer-gridfs-storage');
+const Grid = require('gridfs-stream');
+const methodOverride = require('method-override');
 const ejs = require('ejs');
 const path = require('path');
 
-//Set storage engine
-const storage = multer.diskStorage({
-    destination: './public/uploads/',
-    filename: function(req, file, cb){
-        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
-    }
-});
+//Init app
+const app = express();
 
-//Init upload
+//Middleware
+app.use(bodyParser.json());
+app.use(methodOverride('_method'));
+
+//View Engine setup
+app.set('view engine', 'ejs');
+
+//MongoURI
+const dbURI = "mongodb://leo:abc123@ds135776.mlab.com:35776/mongouploads";
+
+//Create MongoDB connection
+const con = mongoose.createConnection(dbURI);
+
+//Init gfs
+let gfs;
+
+con.once('open', (req, res) => {
+    gfs = Grid(con.db, mongoose.mongo);
+    gfs.collection('uploads');
+})
+
+//Create storage engine
+const storage = new GridFsStorage({
+    url: dbURI,
+    file: (req, file) => {
+      return new Promise((resolve, reject) => {
+        crypto.randomBytes(16, (err, buf) => {
+          if (err) {
+            return reject(err);
+          }
+          const filename = buf.toString('hex') + path.extname(file.originalname);
+          const fileInfo = {
+            filename: filename,
+            bucketName: 'uploads'
+          };
+          resolve(fileInfo);
+        });
+      });
+    }
+  });
 const upload = multer({
     storage: storage,
-    limits: {fileSize: 10000000},
     fileFilter: function(req, file, cb){
         checkFileType(file, cb);
     }
-}).single('myImage');
+});
 
 //Check File Type
 function checkFileType(file, cb){
@@ -36,37 +75,68 @@ function checkFileType(file, cb){
     }
 }
 
-//Init app
-const app = express();
-
-//View Engine setup
-app.set('view engine', 'ejs');
-
-//Public Folder
-app.use(express.static('./public'));
-
-app.get('/', (req, res) => res.render('index'));
-
-app.post('/upload', (req, res) => {
-    upload(req, res, (err) => {
-        if(err){
-            res.render('index', {
-                msg: err
-            });
+//@route GET/
+//@desc loads from
+app.get('/', (req, res) => {
+    gfs.files.find().toArray((err, files) => {
+        //Check if files
+        if(!files || files.length === 0){
+            res.render('index', {files: false});
         } else {
-            if(req.file == undefined){
-                res.render('index', {
-                    msg: 'Error: No Image Selected.'
-                })
-            } else {
-                res.render('index', {
-                    msg: 'File Uploaded.',
-                    file: `uploads/${req.file.filename}`
-                })  
-            }
+            files.map(file => {
+                if(file.contentType === 'image/jpeg' || file.contentType === 'image/jpg' || file.contentType === 'image/png' || file.contentType === 'image/gif'){
+                    file.isImage = true;
+                } else {
+                    file.isImage = false;
+                }
+            });
+            res.render('index', {files: files});
         }
     });
+});
+
+//@route POST/
+//@desc Uploads file into DB
+app.post('/upload', upload.single('file'), (req, res, err) => {
+   // res.json({file: req.file});
+   if(err){
+    res.render('index', {
+        msg: err
+    });
+   } else {
+       if(req.file == undefined){
+           res.render('index', {
+               msg: 'Error: No Image Selected.'
+           })
+       } else {
+           res.redirect('/');
+       }
+   }
 })
+
+//@route GET /images/:filename
+//@desc display all files in json
+app.get('/images/:filename', (req, res) => {
+    gfs.files.findOne({filename: req.params.filename}, (err, file) => {
+        //Check if files
+        if(!file || file.length === 0){
+            return res.status(404).json({
+                err: 'No image exists here. Please upload a few first.'
+            });
+        }
+
+        //Check if image
+        if(file.contentType === 'image/jpeg' || file.contentType === 'image/jpg' || file.contentType === 'image/png' || file.contentType === 'image/gif'){
+            //Read output to browser
+            const readstream = gfs.createReadStream(file.filename);
+            readstream.pipe(res);
+        } else {
+            return res.status(404).json({
+                err: 'Not an image.'
+            });
+        }
+    });
+});
 
 const port = 3000;
 
